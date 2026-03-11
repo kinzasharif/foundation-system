d860c3c0d1de6fce5d233444b0f4d93c79181e51
kinzasharif <kinzasharif790@gmail.com>
2026-03-11 19:56:47 +0500

    .

--- Database/foundation_system.sql
+++ Database/foundation_system.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 03, 2026 at 01:09 AM
+-- Generation Time: Mar 11, 2026 at 02:55 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -32,24 +32,21 @@ CREATE TABLE IF NOT EXISTS `donations` (
   `amount` decimal(10,2) DEFAULT NULL,
   `donation_date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `donations`
 --
 
 INSERT INTO `donations` (`id`, `donor_id`, `amount`, `donation_date`) VALUES
-(1, 2, '3000.00', '2026-03-01'),
+(1, 2, '21312.00', '2026-03-01'),
 (2, 1, '123.00', '2026-03-01'),
-(3, 2, '6000.00', '2026-03-01'),
-(4, 1, '3555.00', '2026-03-01'),
-(5, 1, '55.00', '2026-03-09'),
-(6, 3, '5.00', '2026-03-02'),
-(7, 6, '555.00', '2026-03-02'),
-(8, 9, '6.00', '2026-03-02'),
-(9, 14, '313.00', '2026-03-02'),
-(10, 2, '222.00', '2026-03-02'),
-(11, 2, '4343.00', '2026-03-02');
+(3, 2, '333333.00', '2026-03-01'),
+(4, 1, '44444.00', '2026-03-01'),
+(5, 3, '321.00', '2026-03-06'),
+(6, 4, '222.00', '2026-03-06'),
+(7, 2, '340000.00', '2026-03-07'),
+(8, 2, '777.00', '2026-03-10');
 
 -- --------------------------------------------------------
 
@@ -63,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `donors` (
   `phone` varchar(50) DEFAULT NULL,
   `cnic` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `donors`
@@ -72,8 +69,10 @@ CREATE TABLE IF NOT EXISTS `donors` (
 INSERT INTO `donors` (`id`, `name`, `phone`, `cnic`) VALUES
 (1, 'safeer', '123', '213'),
 (2, 'kinza', '12312', '12312'),
-(7, 'Safeer Ahmad Awan', '23234', '2342342'),
-(14, 'nnnnnn', '222', '22222');
+(3, 'new donor', '1111', '1231'),
+(4, '222', '111', '111'),
+(5, 'kinza sharif', '123123', '6578439'),
+(6, 'shakeel', '123', '123');
 
 -- --------------------------------------------------------
 
@@ -87,7 +86,7 @@ CREATE TABLE IF NOT EXISTS `expenses` (
   `amount` decimal(10,2) DEFAULT NULL,
   `expense_date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `expenses`
@@ -97,15 +96,10 @@ INSERT INTO `expenses` (`id`, `description`, `amount`, `expense_date`) VALUES
 (1, 'Food', '11213.00', '2026-03-01'),
 (2, 'Food', '1231.00', '2026-03-02'),
 (3, 'Medicine', '123.00', '2026-03-31'),
-(4, 'Blood', '123.00', '2026-03-01'),
-(5, 'Medicine', '66.00', '2026-03-01'),
-(6, 'Blood', '222.00', '2026-03-08'),
-(7, 'Food', '33.00', '2026-03-02'),
-(8, 'Blood', '231.00', '2026-03-12'),
-(9, 'Food', '2.00', '2026-03-02'),
-(10, 'Blood', '22.00', '2026-03-02'),
-(11, 'Blood', '22.00', '2026-03-02'),
-(12, 'Medicine', '6.00', '2026-03-02');
+(4, 'Food', '2222.00', '2026-03-02'),
+(5, 'Blood', '2222.00', '2026-03-06'),
+(6, 'Medicine', '65000.00', '2026-03-07'),
+(7, 'Blood', '777.00', '2026-03-10');
 
 -- --------------------------------------------------------
 
@@ -115,28 +109,29 @@ INSERT INTO `expenses` (`id`, `description`, `amount`, `expense_date`) VALUES
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(150) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `picture` varchar(500) DEFAULT NULL,
-  `phone` varchar(50) NOT NULL,
-  `cnic` varchar(50) NOT NULL,
-  `role` varchar(50) DEFAULT 'user',
+  `phone` varchar(20) DEFAULT NULL,
+  `cnic` varchar(30) DEFAULT NULL,
+  `picture` varchar(255) DEFAULT '',
+  `role` enum('user','admin') NOT NULL DEFAULT 'user',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `picture`, `phone`, `cnic`, `role`, `created_at`, `updated_at`) VALUES
-(1, 'safeer', 'safeerahmadawan68208@gmail.com', 'scrypt:32768:8:1$JuGdpEFu1nFz3qei$e9adc76f72a133ba144927ba05da03fbe5b24940f9479b7e5b2790d3e4094fd449bcec17dfa32ac084ac2c2a16450b30d73282e98de8b0edeb844bcdc7a72917', 'uploads/users/safeer_20260303_001820_Screenshot_2026-03-02_231250.png', '213', '123', 'user', '2026-03-02 18:13:00', '0000-00-00 00:00:00'),
-(2, 'admin', 'admin@g.com', 'scrypt:32768:8:1$hFpteQA0seJ8dt0R$e0d33493f61be88e99497c535f25d6aca41fb558e76b0b459c51487ad8f71edb5549ac34c4ee0a348202514e98c701ce5e52d458f81e659cd332ff1f47ba5c04', 'uploads/users/admin_admin_20260303_012509_Screenshot_2026-03-02_231250.png', '123', '12', 'admin', '2026-03-02 20:23:30', '0000-00-00 00:00:00'),
-(3, 'kinza', 'kinza@ggggg.com', 'scrypt:32768:8:1$MhB2Hzei6h8esj7a$5a1313305b02eec3e636bd62ef5b4b520ddc828d6990f4d99889fb8087b20f7d38d8b4abe2a3378368873c866904acd427aaf4bb1ece5a4a91550d136d3e7a17', NULL, '123', '123', 'user', '2026-03-02 21:33:24', '0000-00-00 00:00:00');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `phone`, `cnic`, `picture`, `role`, `created_at`) VALUES
+(1, 'kinza', 'kinzasharif790@gmail.com', 'scrypt:32768:8:1$hTHNq07CejxyFsfJ$55a20899c7ed611f6bcc86a918bfd40803e2f408b2aa0bec67f85249caa6b7c59fa4c9a08718aff21f0ae2ff3df8f152059fbcafe7508504213c4f7dbc61a941', '123', '123', 'uploads/users/kinza_20260302_225901_Screenshot_2026-03-02_225502.png', 'user', '2026-03-02 17:59:02'),
+(2, 'admin', 'admin@gmail.com', 'scrypt:32768:8:1$hTHNq07CejxyFsfJ$55a20899c7ed611f6bcc86a918bfd40803e2f408b2aa0bec67f85249caa6b7c59fa4c9a08718aff21f0ae2ff3df8f152059fbcafe7508504213c4f7dbc61a941', '123', '123', NULL, 'admin', '2026-03-03 01:54:06'),
+(3, 'newuser', 'new@gmail.com', 'scrypt:32768:8:1$hEUVXNPNBPTxIKvN$c9e9dee7f499b1d355cd44f2344b1846531d02c1ca64b9949fb9a5c2fc9430182fa6ece9d6d06c3b879aca038ad76dd7fd78e058ec15bad86b350fa215f8a57c', '123', '123', NULL, 'user', '2026-03-06 09:53:50'),
+(4, 'Arslan Ahmad', 'arslan889@gmail.com', 'scrypt:32768:8:1$t8p31TWDteWyu61U$427becebcb8286357a19c6abdd7b7bfe50e06fa28e13a7cb4c16fc6d95d082fb150fd40bfdd4ce5abefe9e4a0f6cbb55256393f840511a25d2b88d35c4008950', '009', '1234567', NULL, 'user', '2026-03-07 11:16:15'),
+(5, 'shakeel', 'shakeel@ahmed.com', 'scrypt:32768:8:1$AYle28B9s2ps33gW$32d01e373736f1519761927845970507873a4c96b31b4f76aa72b7a0f955e4087abcad83cdbf48c8d5d17fb11b53e6b205e1a183d2a214653fbfc8ba1c4124fb', '123', '123', NULL, 'user', '2026-03-10 15:24:44');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
